031a0424ca1e4193f585ae065b652d6abe04ba23
Paulo Henrique Zancan Zoldan <zoldanpaulo@gmail.com>
2024-02-18 12:50:57 -0300

    criação de nova página

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function toggleMode() {
 
   if (html.classList.contains("light")) {
     img.setAttribute("src", "./assets/avatar-light.png")
-    img.setAttribute("alt", "Foto de Mike Brito sorrindo, com fundo amarelo")
+    img.setAttribute("alt", "Foto de Mike Brito o true da true sorrindo, com fundo amarelo")
   } else {
     img.setAttribute("src", "./assets/avatar-foda.JPG")
     img.setAttribute("alt", "Foto de Paulo Cícero e Guilherme no Jardim Botânico de Curitiba/PR")
