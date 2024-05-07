16dcf843c91a2d6e68a150a3d5afb353de6c985f
Paulo Henrique Zancan Zoldan <zoldanpaulo@gmail.com>
2024-05-06 23:12:31 -0300

    Conclusão ex.015
    Finalização do exercício 15

--- aula-guanabara/Aula12ex/Ex015/script.js
+++ aula-guanabara/Aula12ex/Ex015/script.js
@@ -15,26 +15,35 @@ function calcular() {
             genero = 'Homem'
             if (idade >= 0 && idade < 10) {
                 //Criança
+                img.setAttribute('src', 'foto-bebe-m.png')
             } else if (idade < 21) {
                 //Jovem
+                img.setAttribute('src', 'foto-jovem-m.png')
             } else if (idade < 65) {
                 //Adulto
+                img.setAttribute('src', 'foto-adulto-m.png')
             } else{
                 //Idoso
+                img.setAttribute('src', 'foto-idoso-m.png')
             }
         } else if (fsex[1].checked) {
             genero = 'Mulher'
             if (idade >= 0 && idade < 10) {
                 //Criança
+                img.setAttribute('src', 'foto-bebe-f.png')
             } else if (idade < 21) {
                 //Jovem
+                img.setAttribute('src', 'foto-jovem-f.png')
             } else if (idade < 65) {
                 //Adulto
+                img.setAttribute('src', 'foto-adulto-f.png')
             } else{
                 //Idoso
+                img.setAttribute('src', 'foto-idoso-f.png')
             }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+        res.appendChild(img)
     }
 }
