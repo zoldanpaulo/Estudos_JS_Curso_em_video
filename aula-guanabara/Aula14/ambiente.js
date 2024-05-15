ab9ee5a08f0655f5a79d75200adf6deac28f6013
Paulo Henrique Zancan Zoldan <zoldanpaulo@gmail.com>
2024-05-15 00:29:21 -0300

    Aula 14

---
+++ aula-guanabara/Aula14/ambiente.js
@@ -0,0 +1,15 @@
+/*
+var c = 1
+while (c <= 10) {
+    console.log (`Passo ${c}`)
+    c++
+    
+}
+*/
+
+//----------
+console.log ('Vai começar')
+for (var c = 1;c<=10;c++) {
+    console.log(`Chamado ${c}`)
+}
+console.log ('Isso é tudo pessoal')
